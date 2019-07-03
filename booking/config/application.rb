90602c306bbc1d9e53f28e1fd53d3db576140235
ChocolateAceCream <nuodi@hotmail.com>
2019-07-03 10:51:50 -0400

    finished setting up mongoDB with mongoid

--- booking/config/application.rb
+++ booking/config/application.rb
@@ -30,6 +30,7 @@ module Booking
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    Mongoid.load!("./config/mongoid.yml")
     config.api_only = true
   end
 end
